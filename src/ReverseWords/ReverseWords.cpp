39a44b94e8beda858aa507e55ba38aecac1872ef
Garu <garu1996@yahoo.com>
2025-03-24 21:11:09 +0100

    Homewark2!

--- src/ReverseWords/ReverseWords.cpp
+++ src/ReverseWords/ReverseWords.cpp
@@ -3,19 +3,28 @@
 #include <algorithm>
 #include <ranges>
 #include <windows.h>
+#include <regex>
+
 using namespace std;
 
 // разворачивается вся строка, а не слова в ней и ты возвращаешь не ту переменную
-string reversedWords(const string& aloha){
+string reversedWords(const string &aloha) {
     string alohich = aloha;
+    regex reg("[ ]+");
     ranges::reverse(alohich);
+    auto start = 0;
+    for (auto i = 0; i < alohich.size(); ++i) {
+        if (i == alohich.size() || alohich[i] == ' ') {
+            ranges::reverse (alohich.begin() + start, alohich.begin() + i);
+            start = i+1;
+        }
+    }
     // split -> reverse -> join -> print
     return alohich;
 }
 
 
-auto main()-> int
-{
+auto main() -> int {
     fmt::println(
             "{}\n{}\n{}\n{}",
             reversedWords("A quick brown fox"),
