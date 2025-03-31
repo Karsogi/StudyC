99bf69c63d52218b70f05d7795b59fb2de323774
Garu <garu1996@yahoo.com>
2025-03-31 21:44:41 +0200

    HW3 p1F

--- src/FindingTheLongestString/FindingTheLongestString.cpp
+++ src/FindingTheLongestString/FindingTheLongestString.cpp
@@ -2,10 +2,12 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 namespace v1 {
     string printLongestString(const vector<string> &vec) {
+        assert (!vec.empty() && "The vector should contain some words!");
         return ranges::max(vec, [](const string &a, const string &b) {
             return a.size() < b.size();
         });
@@ -13,6 +15,7 @@ namespace v1 {
 }
 namespace v2 {
     string printLongestString(const vector<string> &vec) {
+        assert (!vec.empty() && "The vector should contain some words!");
         auto maxWord = ranges::max_element(vec, [](const string &a, const string &b) {
             return a.size() < b.size();
         });
@@ -21,6 +24,7 @@ namespace v2 {
 }
 namespace v3 {
     string printLongestString(const vector<string> &vec) {
+        assert (!vec.empty() && "The vector should contain some words!");
         return ranges::min(vec, [](const string &a, const string &b) {
             return a.size() > b.size();
         });
@@ -28,6 +32,7 @@ namespace v3 {
 }
 namespace v4 {
     string printLongestString(const vector<string> &vec) {
+        assert (!vec.empty() && "The vector should contain some words!");
         auto maxWord = ranges::min_element(vec, [](const string &a, const string &b) {
             return a.size() > b.size();
         });
@@ -36,6 +41,6 @@ namespace v4 {
 }
 
 auto main() -> int {
-    auto const ab = vector<string>{"abcd", "lol"};
-    cout << v3::printLongestString(ab);
+    auto const ab = vector<string>{"abcd", "lol","as"};
+    cout << v1::printLongestString(ab);
 }
